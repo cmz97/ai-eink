195f73e60e2c201faaf4aa4c1e312597e4cd3c60
Kevin Zhang <01001010kevin@gmail.com>
2024-03-03 02:55:03 -0800

    A

--- einkDSP.py
+++ einkDSP.py
@@ -66,12 +66,12 @@ class einkDSP:
         device = 0 #Device is the chip select pin. Set to 0 or 1, depending on the connections
         spi = spidev.SpiDev()
         spi.open(bus, device) 
-        spi.max_speed_hz = 10000000 #1MHZ
+        spi.max_speed_hz = 3000000 #1MHZ
         spi.mode = 0
         return spi
 
     def SPI_Delay(self):
-        time.sleep(0.0001)
+        time.sleep(0.00001)
 
     def SPI_Write(self,value):
         return self.spi.xfer2([value])
--- main.py
+++ main.py
@@ -251,13 +251,13 @@ def generate_image():
     if use_eink: 
         eink.epd_w21_init_4g()
         eink.pic_display_4g(hex_pixels)
-        for i in range(100):
-                newBox = draw_text_on_dialog(dialog_image_path, ascii_table_image_path, str(i), text_area_start, text_area_end)
-                curImage.paste(newBox, (0, eink_height-dialogBox.height))
-                hex_pixels = image_to_header_file(curImage)
+        # for i in range(100):
+        #         newBox = draw_text_on_dialog(dialog_image_path, ascii_table_image_path, str(i), text_area_start, text_area_end)
+        #         curImage.paste(newBox, (0, eink_height-dialogBox.height))
+        #         hex_pixels = image_to_header_file(curImage)
 
-                eink.epd_w21_init_4g()
-                eink.pic_display_4g(hex_pixels)
+        #         eink.epd_w21_init_4g()
+        #         eink.pic_display_4g(hex_pixels)s
         eink.epd_sleep()
 
 
