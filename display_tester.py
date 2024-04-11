f88cde7659da9ed1427eedb80e45f4b72c8ee0f2
Kevin Zhang <01001010kevin@gmail.com>
2024-04-10 23:00:43 -0700

    a

--- display_tester.py
+++ display_tester.py
@@ -22,7 +22,7 @@ def dump_1bit(pixels):
         if i % 8 == 0 and i > 0:
             index += 1
         int_pixels[index] |= bit << (7 - (i % 8))
-    return int_pixels.tolist()
+    return int_pixels
 
 myGUI = GUI(240, 416, './Asset/Font/Monorama-Bold.ttf')  # Initialize the GUI
 eink = einkDSP()
@@ -38,7 +38,7 @@ for i in range(1,2):
     myGUI.updateIndex(i % 4,(i-1)% 4)  # Update the index
     myGUI.updateStatusBar(f"CPU {i}% / RAM {i}%", ['./Asset/Image/batt.bmp'])  # Update the status bar
     np_canvas = np.array(myGUI.canvas).astype(np.uint8)
-    np_canvas = dump_1bit(np_canvas)
+    np_canvas = dump_1bit(np_canvas).tolist()
     eink.PIC_display(np_canvas)
     eink.part(np_canvas)
     eink.PIC_display_Clear()
