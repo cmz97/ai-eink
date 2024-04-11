66b12d16f6fe93c80691494c11179c297a7d106b
Kevin Zhang <01001010kevin@gmail.com>
2024-04-10 23:37:25 -0700

    a

--- display_tester.py
+++ display_tester.py
@@ -46,9 +46,17 @@ def dump_1bit(pixels: np.ndarray):
 myGUI = GUI(240, 416, './Asset/Font/Monorama-Bold.ttf')  # Initialize the GUI
 eink = einkDSP()
 
-eink.epd_init_part()
-eink.PIC_display_Clear()
 
+def clear_screen():
+    # self.eink.PIC_display_Clear()
+    image = Image.new("L", (240, 416), "white")
+    pixels = dump_1bit(np.array(image, dtype=np.uint8)).tolist()
+    eink.epd_init_part()
+    eink.PIC_display(pixels)
+    eink.PIC_display_Clear()
+
+eink.epd_init_part()
+clear_screen()
 
 for i in range(1,2):
     time.sleep(0.1)
@@ -57,11 +65,12 @@ for i in range(1,2):
     myGUI.updateStatusBar(f"CPU {i}% / RAM {i}%", ['./Asset/Image/batt.bmp'])  # Update the status bar
     print(f"GUI time: {time.time() - startTime}")
     startTime = time.time()
-
-    np_canvas = np.array(myGUI.canvas).astype(np.uint8)
+    img = myGUI.canvas.copy()
+    img.transpose(Image.FLIP_TOP_BOTTOM)
+    np_canvas = np.array(img).astype(np.uint8)
+    
     np_canvas = dump_1bit(np_canvas)
     # print(np_canvas)
-    eink.epd_init_part()
-    eink.PIC_display_Clear()
+    clear_screen()
     eink.PIC_display(np_canvas)
     print(f"EINK time: {time.time() - startTime}\n")
