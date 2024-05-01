db1fe8ccbcc329be730feb23fd5986fcdf998934
Kevin Zhang <01001010kevin@gmail.com>
2024-04-30 22:33:50 -0700

    Break

--- auto_display.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /home/kevin/ai/ai-eink
-source venv/bin/activate
-python interface-onboard.py display-mode
--- auto_play.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /home/kevin/ai/ai-eink
-venv/bin/python main.py 
